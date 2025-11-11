cb1c81270c0c5c86738dcd4a5f30e40e85146f76
YuhanHou-studio <86899789+YuhanHou-studio@users.noreply.github.com>
2025-11-11 03:08:26 +0000

    Deploying to gh-pages from @ YuhanHou-studio/YuhanHou-studio.github.io@e5d20f2ace9f7d3e1e6d81bbc3c1478cda67e270 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "DouChinn/YuhanHou.github.io",
+    "data-repo": "YuhanHou-studio/YuhanHou-studio.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
